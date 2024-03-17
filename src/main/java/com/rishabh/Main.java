653a6053bb2c323184b8eab3f2b6d09a5f062ad6
Rishabh Singh <rishabsinghrock@gmail.com>
2024-03-17 19:43:56 +0530

    Stereotype annotation for linking class instances

---
+++ src/main/java/com/rishabh/Main.java
@@ -0,0 +1,11 @@
+package com.rishabh;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person p = context.getBean(Person.class);
+        System.out.println(p);
+    }
+}
---
+++ src/main/java/com/rishabh/ProjectConfig.java
@@ -0,0 +1,11 @@
+package com.rishabh;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "com.rishabh")
+public class ProjectConfig {
+
+}
